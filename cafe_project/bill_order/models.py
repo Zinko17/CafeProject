6fc1318761240288388f3024d1f7e7f85aa92624
Zinko17 <vovazin2@gmail.com>
2021-06-09 19:32:41 +0600

    create models: order,bill and meal to order

---
+++ cafe_project/bill_order/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from content.models import Meal
+
+class Order(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    total_sum = models.IntegerField(default=0)
+    status = models.CharField(max_length=30, choices=(
+        ('in_process', 'in_process'),
+        ('closed', 'closed'),
+    ),default='in_process')
+
+
+class Bill(models.Model):
+    order = models.OneToOneField(Order,on_delete=models.SET_NULL,null=True)
+    client_money = models.IntegerField(default=0)
+    payback = models.IntegerField(default=0)
+
+
+
+class MealToOrder(models.Model):
+    meal = models.ForeignKey(Meal,on_delete=models.SET_NULL,null=True)
+    order = models.ForeignKey(Order,on_delete=models.SET_NULL,null=True)
+    quantity = models.IntegerField(default=0)
