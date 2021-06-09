e5e3863b491da3b9f60c972a927a94e5c4724343
Zinko17 <vovazin2@gmail.com>
2021-06-09 19:29:38 +0600

    add register and login paths

--- cafe_project/content/urls.py
+++ cafe_project/content/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('meals',meals_page,name='meals'),
     path('',homepage,name='home'),
     path('detail/<int:category_id>',category_detail,name='detail'),
+    path('register/',register_page,name='register'),
+    path('login/',login_page,name='login'),
 ]
