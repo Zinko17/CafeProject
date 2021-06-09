16c5d192cb43dfd10f85fc0bf5d9dd30aa14ba59
Zinko17 <vovazin2@gmail.com>
2021-06-09 19:30:45 +0600

    add str function

--- cafe_project/content/models.py
+++ cafe_project/content/models.py
@@ -8,11 +8,14 @@ class Category(models.Model):
 
 
 class Meal(models.Model):
+    photo = models.ImageField(default='soup.jpeg')
+    ingredients = models.CharField(max_length=60)
     title = models.CharField(max_length=30)
     price = models.IntegerField()
     gramms = models.IntegerField()
-    photo = models.ImageField(default='soup.jpeg')
-    ingredients = models.CharField(max_length=60)
     category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+
+    def __str__(self):
+        return self.title
 
 
