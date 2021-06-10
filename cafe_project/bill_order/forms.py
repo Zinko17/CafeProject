346f320c85c83bedc0036da7f44bd195c0cbe679
Zinko17 <vovazin2@gmail.com>
2021-06-10 18:14:28 +0600

    create form for Bill view

--- cafe_project/bill_order/forms.py
+++ cafe_project/bill_order/forms.py
@@ -1,8 +1,17 @@
-from django.forms import ModelForm
+from django import forms
 from .models import *
 
 
-class MealToOrderForm(ModelForm):
+
+class MealToOrderForm(forms.ModelForm):
     class Meta:
         model = MealToOrder
         fields = ['meal','quantity']
+
+
+class BillForm(forms.Form):
+    change_status = forms.ChoiceField(choices=(
+        ('yes','yes'),
+        ('no','no'),
+    ))
+    client_money = forms.IntegerField()
