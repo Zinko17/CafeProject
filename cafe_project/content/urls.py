1bbf6e7a2f3b9f5c746971086fdba7e878065e6d
Zinko17 <vovazin2@gmail.com>
2021-06-10 18:16:34 +0600

    add "/"

--- cafe_project/content/urls.py
+++ cafe_project/content/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('meals',meals_page,name='meals'),
+    path('meals/',meals_page,name='meals'),
     path('',homepage,name='home'),
-    path('detail/<int:category_id>',category_detail,name='detail'),
+    path('detail/<int:category_id>/',category_detail,name='detail'),
     path('register/',register_page,name='register'),
     path('login/',login_page,name='login'),
 ]
