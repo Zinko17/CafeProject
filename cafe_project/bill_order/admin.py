d05fb76ec947fcf707d559dd4130ba02a1b181ca
Zinko17 <vovazin2@gmail.com>
2021-06-10 18:12:34 +0600

    add user in OrderAdmin register

--- cafe_project/bill_order/admin.py
+++ cafe_project/bill_order/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from .models import *
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['status','date_created','total_sum']
-    readonly_fields = ['total_sum']
+    list_display = ['user','status','date_created','total_sum']
+    readonly_fields = ['total_sum','user']
 
 admin.site.register(Order,OrderAdmin)
 
