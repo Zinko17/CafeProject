4f0121a16974e29e96cef4e091cd4b93ed1cf1b3
Zinko17 <vovazin2@gmail.com>
2021-06-10 18:16:22 +0600

    add paths close check and check detail

--- cafe_project/bill_order/urls.py
+++ cafe_project/bill_order/urls.py
@@ -2,5 +2,7 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-    path('order/',order_page,name='order')
+    path('order/',order_page,name='order'),
+    path('close_check/<int:order_id>/',close_check_page,name='close_check'),
+    path('check_detail/<int:order_id>/',check_detail,name='check_detail')
 ]
