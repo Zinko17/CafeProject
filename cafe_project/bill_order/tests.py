8338f9eb375f9ff1abe2948ebd257fd07314334b
Zinko17 <vovazin2@gmail.com>
2021-06-10 18:15:57 +0600

    add post test for order

--- cafe_project/bill_order/tests.py
+++ cafe_project/bill_order/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from content.models import Meal,Category
 
 
 class TestOrder(TestCase):
@@ -11,3 +12,24 @@ class TestOrder(TestCase):
     def test_order_get(self):
         self.response = self.client.get(self.url)
         self.assertEqual(self.response.status_code, 200)
+
+
+
+class TestOrderPost(TestCase):
+
+    def setUp(self) -> None:
+        self.url = reverse('order')
+        self.category = Category.objects.create(title='dfsfs')
+        self.meal = Meal.objects.create(ingredients='f', title='fdf', price=200, gramms=200, category=self.category)
+
+    def test_order_post(self):
+
+        data = {
+            'meal':self.meal,
+            'quantity':1
+        }
+        self.response = self.client.post(self.url, data)
+        self.assertEqual(self.response.status_code, 200)
+
+
+
