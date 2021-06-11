afb0bbb5fd8ef4fb5828d3d63ed5f228d521bf1c
Zinko17 <vovazin2@gmail.com>
2021-06-11 18:27:00 +0600

    add paths:meal detail,my orders

--- cafe_project/bill_order/urls.py
+++ cafe_project/bill_order/urls.py
@@ -4,5 +4,7 @@ from .views import *
 urlpatterns = [
     path('order/',order_page,name='order'),
     path('close_check/<int:order_id>/',close_check_page,name='close_check'),
-    path('check_detail/<int:order_id>/',check_detail,name='check_detail')
+    path('check_detail/<int:order_id>/',check_detail,name='check_detail'),
+    path('meal_detail/<int:meal_id>/',meal_detail,name='meal_detail'),
+    path('my_orders/',my_orders,name='my_orders'),
 ]
