d7e47aa2bc58fa68825829eb495471a9debc0e7b
Zinko17 <vovazin2@gmail.com>
2021-06-11 18:26:37 +0600

    add tests: meal detail(post and get),check detail and close check

--- cafe_project/bill_order/tests.py
+++ cafe_project/bill_order/tests.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
 from content.models import Meal,Category
+from .models import *
 
 
 class TestOrder(TestCase):
@@ -33,3 +35,56 @@ class TestOrderPost(TestCase):
 
 
 
+class CloseCheckGetTest(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username='fdsf',password='dfdsflskcms')
+        self.order = Order.objects.create(user=self.user)
+        self.url = reverse('close_check', args=(self.order.id,))
+
+    def test_closecheck_get(self):
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.status_code, 200)
+
+
+class CheckDetailTest(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create_user(username='fdsf', password='dfdsflskcms')
+        self.order = Order.objects.create(user=self.user)
+        check = Bill.objects.create(order=self.order)
+        Bill.objects.get(order__id=self.order.id)
+        self.url = reverse('check_detail', args=(self.order.id,))
+
+    def test_check_detail_get(self):
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.status_code, 200)
+
+
+
+class MealDetailTest(TestCase):
+
+    def setUp(self) -> None:
+        self.category = Category.objects.create(title='dfsfs')
+        self.meal = Meal.objects.create(ingredients='f', title='fdf', price=200, gramms=200, category=self.category)
+        self.url = reverse('meal_detail', args=(self.meal.id,))
+
+    def test_meal_detail_get(self):
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.status_code, 200)
+
+
+
+class MealDetailTestPost(TestCase):
+
+    def setUp(self) -> None:
+        self.category = Category.objects.create(title='dfsfs')
+        self.meal = Meal.objects.create(ingredients='f', title='fdf', price=200, gramms=200, category=self.category)
+        self.url = reverse('meal_detail', args=(self.meal.id,))
+
+    def test_meal_detail_post(self):
+        data = {'text':'fdsfsdfsdg'}
+        User.objects.create_user(username='safad',password='sfasfds')
+        self.client.login(username='safad',password='sfasfds')
+        self.response = self.client.post(self.url,data)
+        self.assertEqual(self.response.status_code, 200)
