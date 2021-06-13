38d5891b001adbba4c55d1a856d3aa4f2af4f263
Zinko17 <vovazin2@gmail.com>
2021-06-13 14:47:14 +0600

    create meal detail and my orders

--- cafe_project/bill_order/views.py
+++ cafe_project/bill_order/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.models import AnonymousUser
 from django.forms import formset_factory
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+from content.models import Meal
+from review.forms import CommentForm
+from review.models import Review
 
 
 def order_page(request):
@@ -23,6 +27,7 @@ def order_page(request):
                     MealToOrder.objects.create(order=order,meal=data['meal'],quantity=data['quantity'])
             order.total_sum = total_sum
             order.save()
+            return redirect('my_orders')
     return render(request,'order_page.html',{'form':form})
 
 
@@ -38,7 +43,6 @@ def close_check_page(request,order_id):
             status_change = form.cleaned_data.get('change_status')
             money = form.cleaned_data.get('client_money')
             payback = money - order.total_sum
-            print(money,order.total_sum,payback)
             if status_change == 'yes' and money >= order.total_sum:
                 Bill.objects.create(order=order,client_money=money,payback=payback)
                 order.status = 'closed'
@@ -55,6 +59,31 @@ def close_check_page(request,order_id):
 def check_detail(request,order_id):
     check = Bill.objects.get(order__id=order_id)
     return render(request,'check_detail.html',{'check':check})
+
+
+
+def meal_detail(request,meal_id):
+    meal = Meal.objects.get(id=meal_id)
+    comment = meal.review_set.all()
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data.get('text')
+            user = request.user
+            Review.objects.create(meal=meal,text=text,user=user)
+
+    return render(request,'meal_detail.html',{'meal':meal,'form':form,'comment':comment})
+
+
+
+def my_orders(request,):
+    user = request.user
+    if isinstance(user, AnonymousUser):
+        return HttpResponse('Please login!')
+    order = Order.objects.filter(user=user)
+    meal_to_order = MealToOrder.objects.filter(order__in=order)
+    return render(request, 'my_orders.html', {'order': order,'mto':meal_to_order})
 
 
 
